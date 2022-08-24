6dedd115ae05b7e0ce2db5e44b46fdfe344440b2
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-24 11:39:30 +0430

    Initial Repo

---
+++ DequeueWithArray/Deque.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DequeueWithArray
+{
+    public class Deque
+    {
+        public int[] queue;
+        public int size;
+        public int startFront;
+        public int startRear;
+        public int endFront;
+        public int endRear;
+
+        public Deque(int sizeOfQueue)
+        {
+            size = sizeOfQueue;
+            queue = new int[sizeOfQueue];
+            startFront = -1;
+            startRear = -1;
+            endFront = sizeOfQueue;
+            endRear = sizeOfQueue;
+        }
+
+        public void StartEnqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The queue is full!");
+
+            }
+            else
+            {
+                startRear++;
+                queue[startRear] = item;
+            }
+        }
+
+        public void EndEnqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The queue is full!");
+            }
+            else
+            {
+                endRear--;
+                queue[endRear] = item;
+            }
+        }
+
+        public void StartDequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                startFront++;
+                Console.WriteLine(queue[startFront]);
+            }
+        }
+
+        public void EndDequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+
+            }
+            else
+            {
+                endFront--;
+                Console.WriteLine(queue[endFront]);
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                for (int i = startFront + 1; i <= startRear; i++)
+                {
+                    Console.WriteLine(queue[i]);
+                }
+
+                for (int i = endFront - 1; i >= endRear; i--)
+                {
+                    Console.WriteLine(queue[i]);
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (startFront == startRear && endFront == endRear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsFull()
+        {
+            if (startRear + 1 == endRear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DequeueWithArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using DequeueWithArray;
+
+var queue = new Deque(10);
+
+queue.StartDequeue();
+queue.StartEnqueue(2);
+queue.StartEnqueue(4);
+queue.StartDequeue();
+queue.StartDequeue();
+
+queue.EndEnqueue(5);
+queue.EndEnqueue(9);
+queue.EndDequeue();
+queue.EndDequeue();
+
+queue.Print();
+
+//queue.Print();
